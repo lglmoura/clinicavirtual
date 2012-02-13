4485ecc7a2c52aced1116ebe88e857393e34ba0b
Luiz Gustavo <lglmoura@gmail.com>
2012-02-13 17:17:26 -0200

    Inicio do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clinicavirtual::Application.config.secret_token = '887a9bd141dde49793c3564602596aa7107d6c2d9f9e4968a0be1d4f2b448d33db258e07be5dee900112bf73edf5fba907f6054758ed6f72ff5517bb28490341'
